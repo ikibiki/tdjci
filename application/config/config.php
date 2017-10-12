af399ef5db0ff4a63334e2ab785960922e4e93b6
tugudush <jerome2kph@gmail.com>
2017-10-12 17:13:12 +0800

    jerome local config

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = '';
 if(ENVIRONMENT=='development'){
 
-    $config['base_url'] = 'http://localhost/tdjci/';
+    $config['base_url'] = 'http://tdjci';
 }
 
 /*
