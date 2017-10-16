6aa55568e679bbe5ad4a3746d710bc5c034b199f
tugudush <jerome2kph@gmail.com>
2017-10-16 07:39:36 -0600

    modified config files

--- application/config/config.php
+++ application/config/config.php
@@ -27,8 +27,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = '';
 if(ENVIRONMENT=='development'){
 
-    $config['base_url'] = 'http://localhost/tdjci/';
+    //$config['base_url'] = 'http://localhost/tdjci/';
 }
+
+$config['base_url'] = 'http://tdj.theshiftleft.com';
 
 /*
 |--------------------------------------------------------------------------
