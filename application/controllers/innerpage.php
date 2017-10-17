ea46cfab10665916741a7ffd56de0edb93657a69
tugudush <jerome2kph@gmail.com>
2017-10-17 22:39:10 +0800

    fixed innnerpage controller

--- application/controllers/innerpage.php
+++ application/controllers/innerpage.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class innerpage extends CI_Controller {
+class Innerpage extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
